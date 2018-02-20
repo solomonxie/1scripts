696f5d9aa29849c7183eebc9d15a9a6a7261bbe1
Solomon Xie <solomonxiewise@gmail.com>
2018-02-21 03:53:18 +0800

    Update from Solomon-mba.local

--- image_to_github.py
+++ image_to_github.py
@@ -9,6 +9,7 @@ then paste in markdown file as a permanent image link.
 
 import os
 import sys
+import json
 import base64
 import requests
 
@@ -16,6 +17,7 @@ def main():
     """
 
     """
+    print 'Start...'
 
     # Convert a image file to base64 string
     if sys.argv[1] is False and os.path.exists(sys.argv[1]) is False:
@@ -24,21 +26,40 @@ def main():
 
     path = sys.argv[1]
 
+    print 'Loading local image file [%s]...' % path
+
     with open(path, 'rb') as f:
         pic = f.read()
 
     bs64 = base64.b64encode(pic)
 
+    print 'Encoded an image into a base64 string. [%d]' % len(bs64)
+
     # upload image data to github
     filename = os.path.basename(path)
     url = 'https://api.github.com/repos/solomonxie/user_content_media/contents/images/'+filename
-    headers = {'authorization': 'token 4f3659f0d896c1f80204548db238bb45b6b806a4'}
+    headers = {'authorization': 'token b1ff0434283fb0610392b7071aad02d3736903fb'}
     payload = '{"message": "auto uploaded by python", "content": "%s"}' % bs64
-    r = requests.request("PUT", url, data=payload, headers=headers)
 
-    print r.status_code
-    print r.content
+    print 'Uploading an image onto Github repository...'
+    r = requests.request("PUT", url, data=payload, headers=headers, timeout=10)
+
+    print 'Response Code: %d' % r.status_code
+    print 'Response body:\n%s' % r.content
+
+    if r.status_code is not 201:
+        print 'Requesting of uploading failed...'
+        return
+
     
+    # get the raw url of this file
+    info = r.json()
+    raw = info['content']['download_url']
+
+    print 'Uploaded.\nNow copy image url[%s] to clipboard...' % raw
+
+    os.system('echo "![Image](%s)" | pbcopy' % raw)
+
 
 
 
