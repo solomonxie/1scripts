eeb397f9a2a7471a39fa7a471d1b9e7128f2d539
Solomon Xie <solomonxiewise@gmail.com>
2018-05-17 14:37:46 +0800

    add author info to the header

--- python3/backing-up-configs.py
+++ python3/backing-up-configs.py
@@ -1,4 +1,10 @@
 # ==== This project should be running in a python3 enviroment, preferred in Virtualenv. ====
+#
+# Author: Solomon Xie
+# Email: solomonxiewise@gmail.com
+# Project: Backing Up Configs
+# Description: Automatically back up important local configuration files to a place.
+#
 
 def main():
     print('hello')
