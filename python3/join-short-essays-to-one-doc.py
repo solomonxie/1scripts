c87c22384d4f631d0da99a1f39cceef7dbfde0e2
Solomon Xie <solomonxiewise@gmail.com>
2018-05-17 17:57:20 +0800

    compelte mission. create "Smart-charset-encoding" algorithm

--- python3/join-short-essays-to-one-doc.py
+++ python3/join-short-essays-to-one-doc.py
@@ -8,7 +8,8 @@
 # Description: Join tons of short essays I wrote long ago to one document
 '''
 
-import os
+from os import path
+from os import walk
 import sys
 import time
 import re
@@ -17,12 +18,48 @@ def main():
     """
     I'm just gonna finish everything in this func
     """
-    path = '/Volumes/SD/Downloads/text/'
+    base_folder = '/Volumes/SD/Downloads/text'
+    archive_folder = '/Volumes/SD/Downloads/text-archive'
 
     ## Walk through the folder
-    for root, subdir, files in os.walk(path, topdown=True):
+    for root, subdir, files in walk(base_folder, topdown=True):
+        #print(root, '\t', subdir, files)
+        #print('root: '+root)
+        #print('subdir: '+','.join(subdir))
+        #print('files: '+','.join(files))
+        #print('finished a loop.\n\n')
+
+        if len(files) < 1: continue
+
+        joint_name = path.basename(path.realpath(root)) + '.md'
+        joint_path = path.join(archive_folder, joint_name)
+
         for name in files:
-            print(name)
+            txt_path = path.join(root, name)
+            txt = smart_reading(txt_path)
+            content = '# '+ name +'\n'+ txt + '\n\n'
+            with open(joint_path, 'a') as f_out:
+                f_out.write(content)
+
+
+
+def smart_reading(path):
+    """
+    Try different encodings
+    """
+    #import pdb; pdb.set_trace()
+
+    charsets = ['utf-8', 'gbk','UTF-16LE']
+
+    for enc in charsets:
+        try:
+            with open(path, 'r', encoding=enc) as f:
+                return f.read()
+        except UnicodeDecodeError:
+            print('Failed loading with encoding [%s] for %s.'%(enc, path))
+            continue
+
+    return 'n/a'
 
 
 if __name__ == "__main__":
