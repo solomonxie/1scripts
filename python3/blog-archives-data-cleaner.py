a2d1adf1ff90d69bf89f0048f30ab57f2b1d482c
Solomon Xie <solomonxiewise@gmail.com>
2018-05-22 01:44:13 +0800

    create script sturcture. it's for cleaning data of my blog & tweets archieves

---
+++ python3/blog-archives-data-cleaner.py
@@ -0,0 +1,45 @@
+# ---coding:utf-8---
+
+# ==== This project should be running in a python3 enviroment, preferred in Virtualenv. ====
+#
+# Author: Solomon Xie
+# Email: solomonxiewise@gmail.com
+# Project: Blog archive data cleaner
+# Description: Extract blog & tweeter data from multiple type of data structures in different files
+#
+
+import re
+import os
+import time
+
+def main():
+    return
+
+
+def diandian_blog_xml():
+    return
+
+def renren_tweets():
+    return
+
+def qq_tweets():
+    return
+
+def sina_weibo_2011():
+    return
+
+
+def sina_weibo_2012():
+    return
+
+def sina_weibo_2013():
+    return
+
+def memes_2008():
+    return
+
+def memes_2011():
+    return
+
+if __name__ == "__main__":
+    main()
