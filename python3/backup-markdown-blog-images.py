9db3dd45620d9571fa28a5d7da6f16cb11986eb8
Solomon Xie <solomonxiewise@gmail.com>
2018-06-03 01:16:27 +0800

    add a new script to backup images

---
+++ python3/backup-markdown-blog-images.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Python3
+
+"""
+File: backup-markdown-blog-images.py
+Author: Solomon Xie
+Email: solomonxiewise@gmail.com
+Github: https://github.com/solomonxie
+Description: Search all images linked in github issues or comments, download to local folder for backing up
+"""
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
