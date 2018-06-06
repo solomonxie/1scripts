d3dc28241310dd150be1075e6cd3e4bc998d27e8
Solomon Xie <solomonxiewise@gmail.com>
2018-06-06 09:47:05 +0800

    finished fetching our daily bread

--- py3/ourdailybread.py
+++ py3/ourdailybread.py
@@ -16,14 +16,83 @@ Workflow:
 """
 
 import requests
-import re
 from bs4 import BeautifulSoup
 
 def main():
-    url = 'https://odb.org'
-    r = requests.get(url)
-    print(r.content)
+    path = './dataset/'
+    odb = OurDailyBread(path)
+    #print(odb.markdown)
+
+    with open(path+'odb.md', 'w') as f:
+        f.write(odb.markdown)
     
+
+class OurDailyBread:
+    """
+    A class for an article from today's odb.org content.  
+    """
+
+    def __init__(self, path):
+        """TODO: to be defined1. """
+        self.url = 'https://odb.org'
+        self.path = ''
+        self.title = ''
+        self.date = ''
+        self.thumbnail = ''
+        self.scripture = ''
+        self.scripture_link = ''
+        self.readbible = ''
+        self.bible_link = ''
+        self.verses = ''
+        self.devotion = ''
+        self.poem = ''
+        self.thought = ''
+        self.insight = ''
+
+        self.fetch(path)
+
+    def fetch(self, path):
+        """TODO: Docstring for fetch.
+        :arg1: TODO
+        :returns: TODO
+        """
+        with open('./dataset/odb.org.html', 'r') as f:
+            soup = BeautifulSoup(f.read(), 'html5lib')
+        #r = requests.get(url)
+        #soup = BeautifulSoup(r.content)
+
+        # Parse contents
+        self.title = soup.find('h2', attrs={'class': 'entry-title'}).get_text()
+        self.date = soup.find('a', attrs={'class': 'calendar-toggle'}).get_text()
+        self.thumbnail = soup.find('img', attrs={'class': 'post-thumbnail'})['src']
+        self.verses = soup.find('div', attrs={'class': 'verse-box'}).get_text()
+        self.devotion = soup.find('div', attrs={'class': 'post-content'}).get_text().strip()
+        self.poem = soup.find('div', attrs={'class': 'poem-box'}).get_text().strip()
+        self.thought = soup.find('div', attrs={'class': 'thought-box'}).get_text().strip()
+        self.insight = soup.find('div', attrs={'class': 'insight-box'}).p.get_text().strip()
+        self.scripture = self.fetch_bible(soup.find('div', attrs={'class': 'passage-box'}))
+
+        body = '# %s (Our daily bread)\n**%s**\n> Verses: %s\n![thumbnail](%s)\n'\
+                +'## Content\n%s\n%s\n%s\n## Scripture\n%s\n## INSIGHT\n> %s'
+        self.markdown = body % (self.title, self.date,self.verses,self.thumbnail, \
+            self.devotion, self.poem, self.thought, self.scripture, self.insight)
+
+        
+    def fetch_bible(self, tag):
+        """TODO: Fetch scriptures from Biblegateway.com
+        :tag: ElementTag instance of BeautifulSoup, as the target tag includes scriptures
+        :type: returns string
+        :returns: Scripture content
+        """
+
+        links = tag.find_all('a')
+        content = 'Read: [%s](%s) | Bible in a Year: [%s](%s)'\
+                %(links[0].get_text(), links[0]['href'], links[1].get_text(), links[1]['href'])
+
+        return content
+
+        
+
 
 if __name__ == "__main__":
     main()
